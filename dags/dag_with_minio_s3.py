3598aaa085df9801d9870a3218fac6d40c742b98
sowmyadevarapu <72220469+dnsowmya@users.noreply.github.com>
2024-03-11 07:12:09 -0500

    Update dag_with_minio_s3.py

--- dags/dag_with_minio_s3.py
+++ dags/dag_with_minio_s3.py
@@ -2,16 +2,22 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+from airflow.utils.email import send_email
+from airflow.operators.python import PythonOperator
+
 
 default_args = {
     'owner': 'dn',
     'retries': 5,
-    'retry_delay': timedelta(minutes=10)
+    'retry_delay': timedelta(minutes=10),
+    'email': ['ndevarapu@outlook.com'],
+    'email_on_failure': True,
+    'email_on_retry': True
 }
 
 with DAG(
-        dag_id = 'dag_with_minio_s3_v02',
-        start_date = datetime(2024, 3, 3),
+        dag_id = 'dag_with_minio_s3_v06',
+        start_date = datetime(2024, 3, 9),
         schedule_interval = '@daily',
         default_args = default_args
 ) as dag:
@@ -22,6 +28,5 @@ with DAG(
         aws_conn_id='minio_conn',
         mode='poke',
         poke_interval=5,
-        timeout=60
-
+        timeout=30
     )
